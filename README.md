c640ac1818c56bf28353db54c93c37c7620a400e
evlgusarov <gusar.zx@gmail.com>
2025-02-18 10:31:23 +0300

    TAGES *Изменил WebDriverManager *Протестировал удаленный запуск тестов на grid

--- README.md
+++ README.md
@@ -4,13 +4,14 @@
 ****
 
 ##### **Запуск по тегам**
-###### 1. Необходимо в config.json указать путь до драйвера, но или включить grid если он настроен
+###### 1. Для локального запуска необходимо в config.json указать путь до драйвера и выставить grid false
+###### 2. Для удаленного запуска необходимо в config.json grid true и указать url
 * Запуск всех регрессионных тестов 
   `mvn clean test -Dgroups=regress`
 * Запуск тестов "Переход между вкладками"
   `mvn clean test -Dgroups=navigation`
 * Запуск тестов "Проверка содержимого в блоках"
-  `mvn clean test -Dgroups=contentItem`
+  `mvn clean test -Dgroups=content`
 * Запуск тестов "Ссылки на сторонний ресурс"
   `mvn clean test -Dgroups=strangerLink`
 * Запуск тестов "Ссылки внутри нашего вэб сервиса"
--- src/main/java/org/utils/WebDriverManager.java
+++ src/main/java/org/utils/WebDriverManager.java
@@ -2,6 +2,7 @@ package org.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -11,40 +12,42 @@ import java.time.Duration;
 
 public class WebDriverManager {
 
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver getDriver() {
-        if (driver == null) {
+        if (driver.get() == null) {
             boolean isGridEnabled = ConfigReader.isGridEnabled();
+            WebDriver newDriver;
 
             if (isGridEnabled) {
                 try {
-                    DesiredCapabilities capabilities = new DesiredCapabilities();
-                    capabilities.setBrowserName(ConfigReader.getBrowser());
-                    capabilities.setVersion(ConfigReader.getBrowserVersion());
-                    driver = new RemoteWebDriver(new URL(ConfigReader.getGridUrl()), capabilities);
+                    ChromeOptions options = new ChromeOptions();
+                    options.setBrowserVersion(ConfigReader.getBrowserVersion());
+                    options.setCapability("browserName", ConfigReader.getBrowser());
+                    newDriver = new RemoteWebDriver(new URL(ConfigReader.getGridUrl()), options);
                 } catch (MalformedURLException e) {
                     throw new RuntimeException("Некорректный URL для Selenium Grid: " + e.getMessage());
                 }
             } else {
                 System.setProperty("webdriver.chrome.driver", ConfigReader.getWebDriverPath());
                 if ("chrome".equalsIgnoreCase(ConfigReader.getBrowser())) {
-                    driver = new ChromeDriver();
+                    newDriver = new ChromeDriver();
                 } else {
                     throw new RuntimeException("Поддерживается только браузер Chrome");
                 }
             }
 
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigReader.getImplicitWait()));
+            newDriver.manage().window().maximize();
+            newDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigReader.getImplicitWait()));
+            driver.set(newDriver);
         }
-        return driver;
+        return driver.get();
     }
 
     public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
         }
     }
 }
